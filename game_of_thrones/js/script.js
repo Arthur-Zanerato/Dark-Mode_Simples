4ebe33aa0453546aa2cf35e15902ef475c9bdc4b
Arthur Aparecido Zanerato <110180271+Arthur-Zanerato@users.noreply.github.com>
2022-12-18 11:43:55 -0300

    Add files via upload

---
+++ game_of_thrones/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('texto').innerText = 'Game of Thrones';
+
+document.querySelector("#btn").addEventListener("click", function(){
+    document.body.classList.toggle("dark-mode");
+});
